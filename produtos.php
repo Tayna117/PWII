0abbad5f2f7acfc881991b263535368bfd7d19f0
Tayna <taynaadrianadasilva33@gmail.com>
2024-10-18 20:48:13 -0300

    aula 18/10

--- produtos.php
+++ produtos.php
@@ -1,68 +1,76 @@
 <?php include "cabecalho.php"; ?>
 
-<?php
-
-if(isset($_GET["pesquisa"]))
+<?php 
+if( isset($_GET["pesquisa"]) )
 {
     $pesquisa = $_GET["pesquisa"];
-    if(empty($pesquisa))
+    if( empty($pesquisa) )
     {
-        //se a variável estiver vazia executa aqui
+       //Se a variavel estiver vazia executa aqui 
+       include "conexao.php";
+       $sql = "Select Id, Descricao, Valor, Codigo_barras, Imagem from Produtos order by Id desc";
+       $resultado = $conexao->query($sql);
+       
+       $conexao->close();
     }
     else
     {
-        //aqui vai a lógica da pesquisa
+        //Aqui vai a lógica da pesquisa
+        include "conexao.php";
+        $sql = "Select Id, Descricao, Valor, Codigo_barras, Imagem 
+                from Produtos  
+                where Descricao like '%$pesquisa%' || Codigo_Barras = '$pesquisa'
+                order by Id desc";
+        $resultado = $conexao->query($sql);
+        
+        $conexao->close();
     }
 }
 else
 {
     $pesquisa = "";
     include "conexao.php";
-    $sql = "select Id, Descricao, Valor, Codigo_barras from Produtos order by Id desc";
+    $sql = "Select Id, Descricao, Valor, Codigo_barras, Imagem from Produtos order by Id desc";
     $resultado = $conexao->query($sql);
-    if ($resultado->num_rows > 0){
-        while($row = $resultado->fetch_assoc()){
-            echo "<tr>";
-            echo "<td>" . $row["Id"] . "</td>";
-            echo "<td>" . $row["Descricao"] . "</td>";
-            echo "<td>" . $row["Valor"] . "</td>";
-            echo "<td><a href='editar_produto.php?id=$row[Id]' class='btn btn-warning' >Editar</a> ";
-            echo "<a class='btn btn-danger'>Excluir</a></td>";
-            echo "</tr>";
-        }
-    }else{
-        echo "<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
-    }
+   
     $conexao->close();
+    
 }
+
+
 ?>
-
-
-
-
-
-
+<br>
+<?php
+    if(isset($_GET["erro"]) && !empty($_POST["erro"]) )
+    {
+        echo "<div class='alert alert-danger'>";
+        echo $_GET["erro"];
+        echo "</div>";
+    }
+?>
+<br>
 <div class="row">
     <div class="col-12">
         <div class="card">
-                <div class="card-header">
+            <div class="card-header">
                 Lista de Produtos
-                </div>
-            <div class= "card-body">
+            </div>
+            <div class="card-body">
                 <div class="row">
                     <div class="col-2">
-                        <a href="novo_produto.php" class="btn btn-success">
+                        <a href="novo_produto.php" class="btn btn-success" >
                             Novo Produto
                         </a>
                     </div>
                     <div class="col-8">
                         <form action="produtos.php" method="get">
                             <div class="input-group mb-3">
-                                <input type="text"
-                                 name="pesquisa"
-                                 value="<?php echo $pesquisa; ?>"
-                                 class="form-control" 
-                                 placeholder="Digite aqui..."/>
+                                <input type="text" 
+                                        name="pesquisa" 
+                                        value="<?php echo $pesquisa; ?>"  
+                                        class="form-control" 
+                                        placeholder="Digite sua pesquisa aqui..." />
+
 
                                 <button class="btn btn-primary" type="submit">
                                     Pesquisar
@@ -75,45 +83,45 @@ else
 
                 <div class="row">
                     <div class="col-12">
-
-                    <table class="table table-hover table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Descrição</th>
-                            <th scope="col">Valor</th>
-                            <th scope="col">Código de barras</th>
-                            <th scope="col">Imagem</th>
-                            <th scope="col"></th>
-                        </tr>
-                     </thead>
-                    <tbody>
-                        <?php for ($i = 0; $i < 25; $i++)
-                        {
-                          echo "<tr>
-                            <td>Id $i</td>
-                            <td>Descrição $i</td>
-                            <td>Valor $i</td>
-                            <td>Código de barras $i</td>
-                            <td>Imagem $i</td>
-                            <td>
-                                <a href='' class='btn btn-warning'>
-                                    Editar
-                                </a>
-                                <a href='' class='btn btn-danger'>
-                                    Excluir
-                                </a>
-                            </td>
-                          </tr>";
-                        }
-                        ?>
-                    </tbody>
-</table>
+                        
+                        <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Descrição</th>
+                                <th scope="col">Valor</th>
+                                <th scope="col">Código de barras</th>
+                                <th scope="col">Imagem</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                            
+                            if ($resultado->num_rows > 0) {
+                                while($row = $resultado->fetch_assoc()) {
+                                    echo "<tr>";
+                                    echo "<td>" . $row["Id"] . "</td>";
+                                    echo "<td>" . $row["Descricao"] . "</td>";
+                                    echo "<td>" . $row["Valor"] . "</td>";
+                                    echo "<td>" . $row["Codigo_barras"] . "</td>";
+                                    echo "<td>" . $row["Imagem"] . "</td>";
+                                    echo "<td><a href='editar_produto.php?Id=$row[Id]' class='btn btn-warning' >Editar</a>  ";
+                                    echo "<a href='excluir_produto.php?Id=$row[Id]' class='btn btn-danger'>Excluir</a></td>";
+                                    echo "</tr>";
+                                }
+                            } else {
+                                echo "<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
+                            }
+                            ?>
+                                                    
+                        </tbody>
+                        </table>
                     </div>
                 </div>
 
             </div>
-        </div>  
+        </div>
     </div>
 </div>
 
